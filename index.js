b0b04a78eca3daa237a90971eaab2daa452ff7c1
Cheyenne Hotchkiss <cheycheymh@gmail.com>
2023-10-21 18:47:55 -0400

    Completed

---
+++ index.js
@@ -0,0 +1,56 @@
+1 + 80;
+60 - 40;
+2 * 3.4;
+5.0 / 2.5;
+let number = 5;
+number += 3;
+number -= 2;
+number *= 10;
+number /= 5;
+
+number = 10;
+
+function add5() {
+    return (number += 5);
+}
+function divideBy3() {
+    return (number /= 3);
+}
+divideBy3();
+add5();
+
+number = 10;
+n === 3.3;
+
+add5();
+divideBy3();
+
+parseFloat("80.123999");
+
+function increment(n) {
+    return n += 1;
+}
+function decrement(n) {
+    return n -= 1;
+}
+
+function add(a, b) {
+    return a + b;
+}
+function subtract(a, b) {
+    return a - b;
+}
+function multiply(a, b) {
+    return a * b;
+}
+function divide(a, b) {
+    return a / b;
+}
+
+function makeInt(n) {
+    return(parseInt(n, 10));
+}
+
+function preserveDecimal(n) {
+    return(parseFloat(n));
+}
